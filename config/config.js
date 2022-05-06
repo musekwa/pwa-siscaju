7de354c334ce50bcbec77e0d553ce5cdcedbee12
musekwa <musekwa2011@gmail.com>
2022-05-06 11:26:28 +0200

    fix refactor hard coded connection string to heroku config var

--- config/config.js
+++ config/config.js
@@ -1,13 +1,11 @@
-import dotenv from 'dotenv'
-dotenv.config()
+import dotenv from "dotenv";
+dotenv.config();
 
 const config = {
   env: process.env.NODE_ENV || "development",
   port: process.env.PORT || 5000,
   jwtSecret: process.env.JWT_SECRET || "meitw2022@estudante",
-  mongoURI:
-    process.env.MONGODB_URI ||
-    "mongodb+srv://musekwa:meitw2022estudante@siscaju.8uwsr.mongodb.net/siscaju_db?retryWrites=true&w=majority",
+  mongoURI: process.env.MONGODB_URI,
 };
 
 export default config;

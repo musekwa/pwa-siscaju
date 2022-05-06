000fcaf9acee3aaaae1c4900bd46579f236de55c
musekwa <musekwa2011@gmail.com>
2022-05-06 10:33:08 +0200

    updated package.json

--- config/db.js
+++ config/db.js
@@ -1,24 +1,11 @@
 process.env.NODE_ENV = "dev";
 import mongoose from "mongoose";
 import config from "./config.js";
-import Mockgoose from "mockgoose";
+
 
 export default {
   connect: async () => {
-    if (process.env.NODE_ENV === "test") {
-      // let mockgoose = new Mockgoose.Mockgoose(mongoose);
-      // mockgoose.prepareStorage().then(function () {
-      //   mongoose
-      //     .connect(config.mongoURI, {
-      //       useNewUrlParser: true,
-      //       useUnifiedTopology: true,
-      //     })
-      //     .then((res) => {
-      //       console.log("Mockgoose is connected for testing");
-      //     })
-      //     .catch((err) => console.log(err));
-      // }).catch((err)=>console.log(err));
-    } else {
+   
       return await mongoose
         .connect(config.mongoURI, {
           useNewUrlParser: true,
@@ -28,7 +15,7 @@ export default {
           console.log("MongoDB is connected successfully!");
         })
         .catch((err) => console.log(err));
-    }
+
   },
 
   close: () => mongoose.disconnect(),
